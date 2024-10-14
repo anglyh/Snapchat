0c9ce184d1a379d5a56fc251fbd5ed4c9992a8c8
anglyh <angel.yaguno@tecsup.edu.pe>
2024-10-14 17:54:56 -0500

    feature/login

---
+++ YagunoSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  YagunoSnapchat
+//
+//  Created by Angel Yaguno on 14/10/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando Iniciar Sesión")
+            if error != nil {
+                print("Se presentó el siguiente error: \(error)")
+            } else {
+                print("Inicio de sesión exitoso")
+            }
+        }
+    }
+}
+
