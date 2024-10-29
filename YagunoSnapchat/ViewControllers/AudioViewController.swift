321405ec0a0d38ebd3f4040e5719310d3b466ece
anglyh <angel.yaguno@tecsup.edu.pe>
2024-10-28 19:54:25 -0500

    audio

---
+++ YagunoSnapchat/ViewControllers/AudioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AudioViewController.swift
+//  YagunoSnapchat
+//
+//  Created by Angel Yaguno on 28/10/24.
+//
+
+import UIKit
+
+class AudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
